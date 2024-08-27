47b658d02a5dea02b88bb0a8596c2fe096965edb
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-08-27 21:30:25 +0900

    chore: Update AuthScreen to conditionally render sign-in or sign-up card

--- slack-tutorial/src/feature/auth/components/auth-screen.tsx
+++ slack-tutorial/src/feature/auth/components/auth-screen.tsx
@@ -3,13 +3,15 @@
 
 import { useState } from "react";
 import { SignInFlow } from "../types";
+import { SignInCard } from "./sign-in-card";
+import { SignUpCard } from "./sign-up-card";
 
 export const AuthScreen = () => {
     const [state, setState] = useState<SignInFlow>("signIn");
     return (
         <div className="h-full flex items-center justify-center bg-[#5C3B58]">
             <div className="md:h-auto md:w-[420px]">
-                Auth screen
+                {state === "signIn" ? <SignInCard /> : <SignUpCard />}
             </div>
         </div>
     )

a1585bb10392080b40daba7f5e9dd113547476df
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-09-02 22:15:28 +0900

    chore: Add GitHub as a provider for Convex authentication

--- slack-tutorial/src/feature/auth/components/sign-in-card.tsx
+++ slack-tutorial/src/feature/auth/components/sign-in-card.tsx
@@ -1,6 +1,3 @@
-import { FaGithub, FaGoogle } from "react-icons/fa";
-
-
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -10,8 +7,10 @@ import {
   CardTitle
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { useAuthActions } from "@convex-dev/auth/react";
 import { Separator } from "@radix-ui/react-separator";
 import { useState } from "react";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 import { SignInFlow } from "../types";
 
 interface SignInCardProps {
@@ -19,8 +18,14 @@ interface SignInCardProps {
 };
 
 export const SignInCard = ({ setState }: SignInCardProps) => {
+  const { signIn } = useAuthActions();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const handleProviderSignIn = (value: "github" | "google") => {
+    signIn(value);
+  };
 
   return (
     <Card className="w-full h-full p-8">
@@ -58,7 +63,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
         <div className="flex flex-col gap-y-2.5">
           <Button
             disabled={false}
-            onClick={() => { }}
+            onClick={() => handleProviderSignIn("google")}
             variant="outline"
             size="lg"
             className="w-full relative"
@@ -68,7 +73,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
           </Button>
           <Button
             disabled={false}
-            onClick={() => { }}
+            onClick={() => handleProviderSignIn("github")}
             variant="outline"
             size="lg"
             className="w-full relative"

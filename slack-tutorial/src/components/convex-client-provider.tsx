2b3419107c50a86a015917b609000d47cbf69abe
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-09-03 08:21:54 +0900

    chore: Refactor ConvexClientProvider to use JSX syntax

--- slack-tutorial/src/components/convex-client-provider.tsx
+++ slack-tutorial/src/components/convex-client-provider.tsx
@@ -7,7 +7,9 @@ import { ReactNode } from "react";
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return <ConvexAuthNextjsProvider client={convex}>
-    {children}
-  </ConvexAuthNextjsProvider>;
+  return (
+    <ConvexAuthNextjsProvider client={convex}>
+      {children}
+    </ConvexAuthNextjsProvider>
+  );
 }

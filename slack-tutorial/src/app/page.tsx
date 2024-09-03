5ebc73c1a549a65ff8fbbeaaac18e7d3811ec5f7
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-09-04 08:02:28 +0900

    chore: Add sign out button to Home component

--- slack-tutorial/src/app/page.tsx
+++ slack-tutorial/src/app/page.tsx
@@ -1,8 +1,18 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { useAuthActions } from "@convex-dev/auth/react";
 
 export default function Home() {
+  const { signOut } = useAuthActions();
+
+
   return (
     <div>
       Logged in!
-    </div>
+      <Button onClick={() => signOut()}>
+        Sign out
+      </Button >
+    </div >
   )
 }

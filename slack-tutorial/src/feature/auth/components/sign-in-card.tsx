50b8ae0196ed1e431c97a47627567ead729a6044
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-09-06 08:07:42 +0900

    chore: Add password provider for Convex authentication and update sign-in form

--- slack-tutorial/src/feature/auth/components/sign-in-card.tsx
+++ slack-tutorial/src/feature/auth/components/sign-in-card.tsx
@@ -9,6 +9,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { useAuthActions } from "@convex-dev/auth/react";
 import { Separator } from "@radix-ui/react-separator";
+import { TriangleAlert } from "lucide-react";
 import { useState } from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 import { SignInFlow } from "../types";
@@ -22,7 +23,19 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const [pending, setPending] = useState(false);
+
+  const onPasswordSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setPending(true);
+    signIn("password", { email, password, flow: "signIn" }).catch(() => {
+      setError("Invalid email or password");
+    }).finally(() => {
+      setPending(false);
+    })
+  }
 
   const onProviderSignIn = (value: "github" | "google") => {
     setPending(true);
@@ -35,12 +48,18 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
         <CardTitle>
           Login to continue
         </CardTitle>
+        <CardDescription>
+          Use your email or another service to continue
+        </CardDescription>
       </CardHeader>
-      <CardDescription>
-        Use your email or another service to continue
-      </CardDescription>
+      {!!error && (
+        <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+          <TriangleAlert className="size-4" />
+          <p>{error}</p>
+        </div>
+      )}
       <CardContent className="space-y-5 px-0 pb-0">
-        <form className="space-y-2.5">
+        <form onSubmit={onPasswordSignIn} className="space-y-2.5">
           <Input
             disabled={pending}
             value={email}

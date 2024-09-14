43c820a88fb1d2b6eea01421bf5e0744c2cf8ee8
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-09-14 09:24:42 +0900

    Enhance workspace creation logic with improved error handling and loading states

--- slack-tutorial/src/feature/workspaces/components/create-workspace-modal.tsx
+++ slack-tutorial/src/feature/workspaces/components/create-workspace-modal.tsx
@@ -1,32 +1,28 @@
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
 import { useCreateWorkspace } from "../api/use-create-workspace";
 import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal";
 
 export const CreateWorkspaceModal = () => {
   const [open, setOpen] = useCreateWorkspaceModal();
+  const [name, setName] = useState("");
 
-  const { mutate } = useCreateWorkspace();
+  const { mutate, isPending } = useCreateWorkspace();
 
   const handleClose = () => {
     setOpen(false);
   }
 
-  const handleSubmit = () => {
-    mutate({
-      name: "Workspace 1"
-    }, {
-      onSuccess() {
-        // Redirect to that workspace id
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    mutate({ name }, {
+      onSuccess(data) {
+        console.log(data);
       },
-      onError: () => {
-        // Show toast error
-      },
-      onSettled: () => {
-        // Reset form
-      }
-    })
+    });
   };
 
   return (
@@ -35,17 +31,18 @@ export const CreateWorkspaceModal = () => {
         <DialogHeader>
           <DialogTitle>Add a workspace</DialogTitle>
         </DialogHeader>
-        <form className="space-y-4">
+        <form onSubmit={handleSubmit} className="space-y-4">
           <Input
-            value=""
-            disabled={false}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            disabled={isPending}
             required
             autoFocus
             minLength={3}
             placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
           />
           <div className="flex justify-end" >
-            <Button disabled={false}>
+            <Button disabled={isPending}>
               Create
             </Button>
           </div>

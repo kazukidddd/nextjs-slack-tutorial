1c2b88afb55a749b26a4a10433deb36af18e888f
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-09-17 08:08:55 +0900

    Add ChannelIdPage, WorkspaceIdLayout, and Toolbar components

---
+++ slack-tutorial/src/app/workspace/[workspaceId]/layout.tsx
@@ -0,0 +1,16 @@
+import { Toolbar } from "./toolbar";
+
+interface WorkspaceIdLayoutProps {
+  children: React.ReactNode;
+};
+
+const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
+  return (
+    <div className="h-full">
+      <Toolbar />
+      {children}
+    </div>
+  );
+}
+
+export default WorkspaceIdLayout;
---
+++ slack-tutorial/src/app/workspace/[workspaceId]/toolbar.tsx
@@ -0,0 +1,7 @@
+export const Toolbar = () => {
+  return (
+    <div className="bg-red-500">
+      Toolbar
+    </div>
+  );
+};

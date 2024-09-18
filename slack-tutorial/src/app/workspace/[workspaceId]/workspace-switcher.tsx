db642d9558f9d32e5aedf9524066140ae640d5be
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-09-19 08:09:31 +0900

    Refactor WorkspaceSwitcher to use DropdownMenu for improved navigation

--- slack-tutorial/src/app/workspace/[workspaceId]/workspace-switcher.tsx
+++ slack-tutorial/src/app/workspace/[workspaceId]/workspace-switcher.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+
 export const WorkspaceSwitcher = () => {
   return (
-    <div>
-      Workspace switcher
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button className="size-9 relative overflow-hidden bg-[#ABABAD] hover:bg-[#ABABAD]/80 text-slate-800 font-semibold text-xl">
+          A
+        </Button>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
   );
 }

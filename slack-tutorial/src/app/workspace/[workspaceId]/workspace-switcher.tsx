0afe4e1bd2c9c5b4416677e7f755f17125b49b62
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-09-24 08:26:32 +0900

    Refactor WorkspaceSwitcher to use DropdownMenu and integrate client-side functionality

--- slack-tutorial/src/app/workspace/[workspaceId]/workspace-switcher.tsx
+++ slack-tutorial/src/app/workspace/[workspaceId]/workspace-switcher.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { useGetWorkspace } from "@/feature/workspaces/api/use-get-workspace";
 import { useGetWorkspaces } from "@/feature/workspaces/api/use-get-workspaces";
@@ -23,14 +24,14 @@ export const WorkspaceSwitcher = () => {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className="size-9 relative overflow-hidden bg-[#ABABAD] hover:bg-[#ABABAD]/80 text-slate-800 font-semibold text-xl">
-        {/* <Button className="size-9 relative overflow-hidden bg-[#ABABAD] hover:bg-[#ABABAD]/80 text-slate-800 font-semibold text-xl"> */}
-        {workspaceLoading ? (
-          <Loader className="size-5 animate-spin shrink-0" />
-        ) : (
-          workspace?.name.charAt(0).toUpperCase()
-        )}
-        {/* </Button> */}
+      <DropdownMenuTrigger asChild>
+        <Button className="size-9 relative overflow-hidden bg-[#ABABAD] hover:bg-[#ABABAD]/80 text-slate-800 font-semibold text-xl">
+          {workspaceLoading ? (
+            <Loader className="size-5 animate-spin shrink-0" />
+          ) : (
+            workspace?.name.charAt(0).toUpperCase()
+          )}
+        </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent side="bottom" align="start" className="w-64">
         <DropdownMenuItem

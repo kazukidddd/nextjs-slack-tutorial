ef86772a316a4a3cf66fca80df3b7c6a320beb70
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-09-28 15:14:41 +0900

    Refactor workspace sidebar to include member and workspace data

--- slack-tutorial/src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ slack-tutorial/src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,9 +1,38 @@
+"use client";
+
+import { useCurrentMember } from "@/feature/members/api/use-current-member";
+import { useGetWorkspace } from "@/feature/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { AlertTriangle, Loader } from "lucide-react";
 
 export const WorkspaceSidebar = () => {
   const workspaceId = useWorkspaceId();
 
+  const { data: member, isLoading: memberLoading } = useCurrentMember({ workspaceId });
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId });
+
+  if (workspaceLoading || memberLoading) {
+    return (
+      <div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
+        <Loader className="size-5 animate-spin text-white" />
+      </div>
+    );
+  }
+
+  if (!workspace || !member) {
+    return (
+      <div className="flex flex-col gap-y-2 bg-[#5E2C5F] h-full items-center justify-center">
+        <AlertTriangle className="size-5 animate-spin text-white" />
+        <p className="text-white text-sm">
+          Workspace not found
+        </p>
+      </div>
+    );
+  }
+
   return (
-    <div>Workspace Sidebar</div>
+    <div className="flex flex-col bg-[#5E2C5F] h-full">
+
+    </div>
   )
 };

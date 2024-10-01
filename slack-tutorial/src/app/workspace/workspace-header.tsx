19a4933483e96b4498617ea883a8c22818bc82d5
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-10-01 17:51:29 +0900

    Refactor workspace header to include workspace name and active status

--- slack-tutorial/src/app/workspace/workspace-header.tsx
+++ slack-tutorial/src/app/workspace/workspace-header.tsx
@@ -28,6 +28,10 @@ export const WorkspaceHeader = ({ workspace }: WorkspaceHeaderProps) => {
             <div className="size-9 relative overflow-hidden bg-[#616061] text-white font-semibold text-xl rounded-md flex items-center justify-center mr-2">
               {workspace.name.charAt(0).toUpperCase()}
             </div>
+            <div className="flex flex-col items-start">
+              <p className="font-bold" >{workspace.name}</p>
+              <p className="text-xs text-muted-foreground">Active workspace</p>
+            </div>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>

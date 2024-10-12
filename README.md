e0b263f82e27e6789f4c35b5dceb69465187adfd
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-10-12 22:27:47 +0900

    Refactor workspace header to include TrashIcon for deleting workspaces Refactor workspace header to include DialogHeader component for preferences modal Refactor workspace header to include PreferencesModal component for workspace preferences Refactor workspace header to include JotaiProvider for state management and update icon import Add JotaiProvider component for state management Update cSpell.words in .vscode/settings.json Update README.md with getting started instructions and add components Add update mutation for workspaces in workspaces.ts Update use-create-workspace.ts to handle error status Add use-update-workspace.ts for updating workspaces

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 Slack Clone With Nextjs, React, Tailwind, Auth.js
 
 https://www.youtube.com/watch?app=desktop&si=GTZ35xUopqoNLOvF&v=lXITA5MZIiI&feature=youtu.be
+
+## Getting Started
+
+cd slack-tutorial
+bunx convex dev # to run the server
+
+another terminal
+cd slack-tutorial
+bun run dev # to run the server
+
+add components
+bunx --bun shadcn@latest add tooltip
--- slack-tutorial/convex/workspaces.ts
+++ slack-tutorial/convex/workspaces.ts
@@ -94,3 +94,34 @@ export const getById = query({
     return await ctx.db.get(args.id);
   }
 });
+
+export const update = mutation({
+  args: {
+    id: v.id("workspaces"),
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw  Error("Unauthorized");
+    }
+
+    const member = await ctx.db
+    .query("members")
+    .withIndex("by_workspace_id_user_id", (q) => 
+      q.eq("workspaceId", args.id).eq("userId", userId),
+    )  
+    .unique();
+
+    if (!member || member.role !== "admin") {
+      throw new Error("Unauthorized");
+    }
+
+    await ctx.db.patch(args.id, {
+      name: args.name,
+    });
+
+    return args.id;
+  },
+});

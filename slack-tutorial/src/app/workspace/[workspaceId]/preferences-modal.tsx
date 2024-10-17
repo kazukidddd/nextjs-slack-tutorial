56e9a42ff72618a50aeef71c85204146dd784b42
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-10-17 23:02:25 +0900

    Refactor workspace header to include edit functionality for workspace name

--- slack-tutorial/src/app/workspace/[workspaceId]/preferences-modal.tsx
+++ slack-tutorial/src/app/workspace/[workspaceId]/preferences-modal.tsx
@@ -1,5 +1,7 @@
 
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 import { useUpdateWorkspace } from "@/feature/workspaces/api/use-update-workspace";
 import { TrashIcon } from "lucide-react";
 import { useState } from "react";
@@ -16,8 +18,10 @@ export const PreferencesModal = ({
   initialValue
 }: PreferencesModalProps) => {
   const [value, setValue] = useState(initialValue);
+  const [editOpen, setEditOpen] = useState(false);
 
   const { mutate: updateWorkspace, isPending: isUpdatingWorkspace } = useUpdateWorkspace();
+  const { mutate: removeWorkspace, isPending: isRemovingWorkspace } = useUpdateWorkspace();
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className="p-0 bg-gray-50 overflow-hidden">
@@ -27,19 +31,52 @@ export const PreferencesModal = ({
           </DialogTitle>
         </DialogHeader>
         <div className="px-4 pb-4 flex flex-col gap-y-2">
-          <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
-            <div className="flex items-center justify-between">
-              <p className="text-sm font-semibold">
-                Workspace name
-              </p>
-              <p className="text-sm text-[#1264a3] hover:underline font-semibold">
-                Edit
-              </p>
-            </div>
-            <p className="text-sm">
-              {value}
-            </p>
-          </div>
+          <Dialog open={editOpen} onOpenChange={setEditOpen}>
+            <DialogTrigger asChild>
+              <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+                <div className="flex items-center justify-between">
+                  <p className="text-sm font-semibold">
+                    Workspace name
+                  </p>
+                  <p className="text-sm text-[#1264a3] hover:underline font-semibold">
+                    Edit
+                  </p>
+                </div>
+                <p className="text-sm">
+                  {value}
+                </p>
+              </div>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>
+                  Rename this workspace
+                </DialogTitle>
+              </DialogHeader>
+              <form className="space-y-4" onSubmit={() => { }}>
+                <Input
+                  value={value}
+                  disabled={isUpdatingWorkspace}
+                  onChange={(e) => setValue(e.target.value)}
+                  required
+                  autoFocus
+                  minLength={3}
+                  maxLength={80}
+                  placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+                />
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button variant="outline" disabled={isUpdatingWorkspace}>
+                      Cancel
+                    </Button>
+                  </DialogClose>
+                  <Button disabled={isUpdatingWorkspace}>
+                    Save
+                  </Button>
+                </DialogFooter>
+              </form>
+            </DialogContent>
+          </Dialog>
           <button
             disabled={false}
             onClick={() => { }}

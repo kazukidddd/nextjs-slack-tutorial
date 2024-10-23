4cbe3adb960e8325b866acc598a0157d38f58454
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-10-23 21:48:58 +0900

    Refactor sidebar-item component to add variant prop for styling

--- slack-tutorial/src/app/workspace/[workspaceId]/sidebar-item.tsx
+++ slack-tutorial/src/app/workspace/[workspaceId]/sidebar-item.tsx
@@ -1,14 +1,29 @@
 import { Button } from "@/components/ui/button";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { cva, VariantProps } from "class-variance-authority";
 import { LucideIcon } from "lucide-react";
 import Link from "next/link";
 import { IconType } from "react-icons/lib";
 
+
+const sidebarItemVariants = cva(
+  "flex items-center gap-1.5 justify-center font-normal h-7 px-[18px] text-sm overflow-hidden ",
+  {
+    variants: {
+      variant: {
+        default: "text-[#f9edffcc]",
+        active: "text-[#481349] bg-white/90 hover:bg-white/90",
+      }
+    }
+  }
+);
+
 interface SidebarItemProps {
   label: string;
   id: string;
-  icon: LucideIcon | IconType
-}
+  icon: LucideIcon | IconType;
+  variant?: VariantProps<typeof sidebarItemVariants>["variant"];
+};
 
 export const SidebarItem = ({
   label,

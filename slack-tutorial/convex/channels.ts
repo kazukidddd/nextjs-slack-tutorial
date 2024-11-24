168e74e4ff221cd66c7c914f91a12450a585b95f
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-11-24 21:02:03 +0900

    Comment out unused imports and the get function in channels.ts

--- slack-tutorial/convex/channels.ts
+++ slack-tutorial/convex/channels.ts
@@ -1,12 +1,12 @@
-import { v } from "convex/values";
-import { query } from "./_generated/server";
-import { auth } from "./auth";
+// import { v } from "convex/values";
+// import { query } from "./_generated/server";
+// import { auth } from "./auth";
 
-export const get = query({
-  args: {
-    workspaceId: v.id("workspaces"),
-  },
-  handler: async (ctx, args) => {
-    const userId = await auth
-  },
-});
+// export const get = query({
+//   args: {
+//     workspaceId: v.id("workspaces"),
+//   },
+//   handler: async (ctx, args) => {
+//     const userId = await auth
+//   },
+// });
